03f3098e9cc6d889f410477cb65ca0ba3faf1dad
İsmail Yarar <ismileyarar@gmail.com>
2024-10-08 18:33:24 +0300

    footer updated

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,6 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-    {/*   &copy; {new Date().getFullYear()} Takuya Matsuyama. All Rights Reserved. */}
     </Box>
   )
 }
